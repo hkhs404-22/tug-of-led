4a91f4d5188f3a1bcda910e4b5f10663fdaeb4dc
hkhs404-22 <131227218+hkhs404-22@users.noreply.github.com>
2023-05-17 11:55:53 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,23 @@
 input.onButtonPressed(Button.A, function () {
-    rope += -0.1
+    rope += -0.3
 })
 input.onButtonPressed(Button.B, function () {
-    rope += 0.1
+    rope += 0.3
 })
 let rope = 2
 music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once)
+if (rope < 0) {
+    music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
+} else if (rope > 4) {
+    music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+}
 basic.forever(function () {
     basic.clearScreen()
     led.plot(Math.round(rope), 2)
     if (rope < 0) {
         basic.showString("A win")
-        music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
     } else if (rope > 4) {
         basic.showString("B win")
-        music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
     } else {
         basic.showString(" Keep it up!")
     }
